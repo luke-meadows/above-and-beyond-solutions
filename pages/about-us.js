e06cb72d4736528215f07e82d205c064e74d1d0f
luke-meadows <lukemeadowsdev@gmail.com>
2022-02-18 15:54:29 +0000

    class name

--- pages/about-us.js
+++ pages/about-us.js
@@ -23,7 +23,7 @@ export default function AboutUs() {
           client expectations.
           <br />
           <br />
-          <strong>
+          <strong style={{ fontWeight: '500' }}>
             We make it our priority to understand our client&apos;s objectives,
             working alongside clients from concept to completion to create a
             unique and memorable event.
