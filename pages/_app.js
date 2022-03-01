c497228ac566bc23849552e1b96ea11fbd9b62a3
luke-meadows <lukemeadowsdev@gmail.com>
2022-03-01 12:13:05 +0000

    app

--- pages/_app.js
+++ pages/_app.js
@@ -2,20 +2,10 @@ import '../styles/globals.css';
 import '../styles/fontello/css/fontello.css';
 import Page from '../components/Page';
 import { CtxProvider } from '../lib/ctxProvider';
-import Head from 'next/head';
 
-function MyApp({ Component, pageProps, logo }) {
+function MyApp({ Component, pageProps }) {
   return (
     <CtxProvider>
-      <Head>
-        <title>Above & Beyond Solutions</title>
-        <meta
-          name="description"
-          content="Small agency care, with big agency capabilities"
-        />
-        <meta property="og:image" content={logo} />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <Page>
         <Component {...pageProps} />
       </Page>
@@ -25,12 +15,11 @@ function MyApp({ Component, pageProps, logo }) {
 
 MyApp.getInitialProps = async function ({ Component, ctx }) {
   let pageProps = {};
-  const logo = fetch('../public/assets/logos/thumbnail-white-logo.svg');
   if (Component.getInitialProps) {
     pageProps = await Component.getInitialProps(ctx);
   }
   pageProps.query = ctx.query;
-  return { pageProps, logo };
+  return { pageProps };
 };
 
 export default MyApp;
