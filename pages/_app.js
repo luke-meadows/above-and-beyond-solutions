e74e2377a8b6d997f734dde43b55fc4ed80bf154
luke-meadows <lukemeadowsdev@gmail.com>
2022-03-01 11:43:10 +0000

    logo

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,21 @@ import '../styles/globals.css';
 import '../styles/fontello/css/fontello.css';
 import Page from '../components/Page';
 import { CtxProvider } from '../lib/ctxProvider';
+import Head from 'next/head';
+import logo from '../public/assets/logos/thumbnail-white-logo.svg';
 
 function MyApp({ Component, pageProps }) {
   return (
     <CtxProvider>
+      <Head>
+        <title>Above & Beyond Solutions</title>
+        <meta
+          name="description"
+          content="Small agency care, with big agency capabilities"
+        />
+        <meta property="og:image" content={logo} />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Page>
         <Component {...pageProps} />
       </Page>
