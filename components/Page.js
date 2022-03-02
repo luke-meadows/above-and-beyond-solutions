3939f1340c39053c0c1591b5bbf9f0cfc489a708
luke-meadows <lukemeadowsdev@gmail.com>
2022-03-02 16:36:25 +0000

    seo

--- components/Page.js
+++ components/Page.js
@@ -14,23 +14,22 @@ export default function Page({ children }) {
   return (
     <StyledPage>
       <NextSeo
-        title="Using More of Config"
-        description="This example uses more of the available config options."
-        canonical="https://www.canonical.ie/"
+        title="Above & Beyond Solutions"
+        description="Small agency care, with big agency capabilities."
         openGraph={{
           url: 'https://www.url.ie/a',
           title: 'Open Graph Title',
           description: 'Open Graph Description',
           images: [
             {
-              url: 'https://www.example.ie/og-image-01.jpg',
+              url: 'https://scontent-lhr8-2.xx.fbcdn.net/v/t1.6435-9/144375383_10159109711442148_8884310596273802723_n.jpg?_nc_cat=102&ccb=1-5&_nc_sid=09cbfe&_nc_eui2=AeHXbBoknZxF3LPAe06GIAyZjS-FR8Dfn0WNL4VHwN-fRR2o6CrDU2JH57-11EgkVw8&_nc_ohc=2JTK0WIWo6MAX8RlbUt&_nc_ht=scontent-lhr8-2.xx&oh=00_AT-Lzm-db4iEtkwjWviXrSAOjz7utm1TwhUcG9GoId63Gw&oe=62437B05',
               width: 800,
               height: 600,
               alt: 'Og Image Alt',
               type: 'image/jpeg',
             },
             {
-              url: 'https://www.example.ie/og-image-02.jpg',
+              url: 'https://scontent-lhr8-2.xx.fbcdn.net/v/t1.6435-9/144375383_10159109711442148_8884310596273802723_n.jpg?_nc_cat=102&ccb=1-5&_nc_sid=09cbfe&_nc_eui2=AeHXbBoknZxF3LPAe06GIAyZjS-FR8Dfn0WNL4VHwN-fRR2o6CrDU2JH57-11EgkVw8&_nc_ohc=2JTK0WIWo6MAX8RlbUt&_nc_ht=scontent-lhr8-2.xx&oh=00_AT-Lzm-db4iEtkwjWviXrSAOjz7utm1TwhUcG9GoId63Gw&oe=62437B05',
               width: 900,
               height: 800,
               alt: 'Og Image Alt Second',
