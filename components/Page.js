ac1789a1efc3735c3942ec61c6ff34dcdda11b96
luke-meadows <lukemeadowsdev@gmail.com>
2022-03-02 15:35:45 +0000

    meta

--- components/Page.js
+++ components/Page.js
@@ -5,30 +5,12 @@ import MobileNav from './MobileNav';
 import { Ctx } from '../lib/ctxProvider';
 import { useContext } from 'react';
 import { GlobalStyle } from '../styles/GlobalStyles';
-import { NextSeo } from 'next-seo';
 
 export default function Page({ children }) {
   const { mobileNavActive } = useContext(Ctx);
 
   return (
     <StyledPage>
-      <NextSeo
-        openGraph={{
-          url: 'https://www.aboveandbeyondsolutions.co.uk/',
-          title: 'Above And BEYOND',
-          description: 'AA',
-          images: [
-            {
-              url: 'https://www.aboveandbeyondsolutions.co.uk/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fthumbnail-white-logo.e8da362a.svg&w=1920&q=75',
-              width: 800,
-              height: 600,
-              alt: 'Og Image Alt',
-              type: 'image/jpeg',
-            },
-          ],
-          site_name: 'SiteName',
-        }}
-      />
       {mobileNavActive && <OverLay />}
 
       <MobileNav />
--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,5 @@
 import Document, { Html, Head, NextScript, Main } from 'next/document';
-
+import logo from '/public/assets/logos/thumbnail-black-logo.svg;';
 import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
@@ -33,6 +33,10 @@ export default class MyDocument extends Document {
       <Html lang="en-GB">
         <Head>
           <title>Above & Beyond Solutions</title>
+          <meta property="og:type" content="website" />
+          <meta property="og:title" content="Above && Beyond Solutions" />
+          {/* <meta property="og:description" content={ogDescription} /> */}
+          <meta property="og:image" content={logo} />
           <meta
             name="description"
             content="Small agency care, with big agency capabilities"
