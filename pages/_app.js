f2ca4d45339b686ee6aa49101501096e250d683e
luke-meadows <lukemeadowsdev@gmail.com>
2022-12-19 18:20:23 +0000

    gallery hotfix gtags

--- pages/_app.js
+++ pages/_app.js
@@ -17,7 +17,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       {/* <Provider value={client}> */}
-      {/* <Script id="google-tag-manager" strategy="afterInteractive">
+      <Script id="google-tag-manager" strategy="afterInteractive">
         {`
         (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
@@ -25,7 +25,7 @@ function MyApp({ Component, pageProps }) {
         'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
       })(window,document,'script','dataLayer','G-0XQDQYLL2Z');
       `}
-    </Script> */}
+      </Script>
       <CtxProvider>
         <Page>
           <Component {...pageProps} />
